fd91a00920362a48aca64ef76704eedaf80d0135
m.ibrahim <mohammad.si.tt@gmail.com>
2022-04-25 00:52:48 +0300

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+]
+function superbowlWin(arrayOfObjects){
+    const winOb = arrayOfObjects.find(ob => ob.result === 'W')
+    if (winOb){
+        return winOb.year;
+    } else {
+        return undefined;
+    }
+     
+}
+
+console.log(superbowlWin(record))
+
